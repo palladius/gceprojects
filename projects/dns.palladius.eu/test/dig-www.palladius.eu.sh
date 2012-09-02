7a8426c1a8410f12f9942b78f410137ddd6c1879
Riccardo GCarlesso <riccardo.carlesso@gmail.com>
2012-09-02 12:51:31 +0100

    new tests

--- projects/dns.palladius.eu/test/dig-www.palladius.eu.sh
+++ projects/dns.palladius.eu/test/dig-www.palladius.eu.sh
@@ -1,6 +1,7 @@
 
 # this test must not fail:
 
-dig www.palladius.eu @__HOSTNAME__
+#dig www.palladius.eu @__HOSTNAME__
 
 assert '42.42.42.42' 'dig test.palladius.eu @__HOSTNAME__'
+assert '192.168.108.42' 'dig test.hetzner.palladius.eu @__HOSTNAME__'
